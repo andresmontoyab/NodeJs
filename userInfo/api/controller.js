eb4d306eb8f5371978c8945c70d2a3a38e68121e
andresmontoyab <and.montoya18@gmail.com>
2019-05-31 11:18:02 -0500

    Implementando get y delete endpoint

--- userInfo/api/controller.js
+++ userInfo/api/controller.js
@@ -1,15 +1,43 @@
 'use strict';
 
+var userInfoArray = [];
+
 var controllers = {
     userInfo: function(req, res) {
-       console.log(req.body);
-       var userInfo = {
-           playerId: "Let's Play",
-           level: "1",
-           equipment: ":(",
+
+        // Extraer informacion del Json
+        const infoUser = {
+           playerId: req.body.playerId,
+           region: req.body.region,
+           gameId: req.body.gameId
        }
-       res.json(userInfo);
+
+       // Adicional Informacion a un Array en memoria
+       userInfoArray.push(infoUser);       
+
+       //Retornar Info
+       res.json(infoUser);
    },
+
+   deleteUserInfo: function(req, res) {
+
+        // Extraer ID usuario
+        const userId = req.body.playerId;
+
+        let indice = -1;
+        indice = userInfoArray.findIndex(user => user.playerId = userId);
+        console.log(indice);
+        if (indice >= 0) {
+            userInfoArray.splice(indice,1);
+            res.status(200).json("Usuario Eliminado");
+        } else {
+            res.status(400).json("El usuario no existe");
+        }
+   },
+
+   buscarUsuarios: function(req, res) {
+       res.json(userInfoArray);
+   }
 };
 
 module.exports = controllers;
--- userInfo/api/routes.js
+++ userInfo/api/routes.js
@@ -3,7 +3,13 @@
 var controller = require('./controller');
 
 module.exports = function(app) {
-   //  Guardar Info User
-   app.route('/userInfo')
+    //  Guardar Info User
+    app.route('/userInfo')
        .post(controller.userInfo);
+    
+    app.route('/deleteUser')
+        .delete(controller.deleteUserInfo);
+
+    app.route('/getUser')
+        .get(controller.buscarUsuarios);
 };
